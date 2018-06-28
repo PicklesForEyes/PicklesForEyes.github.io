85299f46396808dc5592df68b1f9613751d15894
Benjamin Lawson <lawbe101@yahoo.com>
2018-06-28 16:45:58 -0400

    added styling and animation to clickables

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,15 @@ $(document).ready(() => {
   $('#lostffound').on('click', e => {
     e.preventDefault();
     $('#lostffound-description').toggleClass('visibility visible')
+    $('#lostffound, #lostffound .card-image').css(
+      {'transition': 'all .25s ease-in', 'border-radius': '0rem'})
   })
 
   // show/hide djinn
   $('#djinn').on('click', e => {
     e.preventDefault();
     $('#djinn-description').toggleClass('visibility visible');
+    $('#djinn, #djinn .card-image').css(
+      {'transition': 'all .25s ease-in', 'border-radius': '0rem'})
   })
 })
